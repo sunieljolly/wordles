197a282a87374af8d48a2bbded4d161b3a27ab51
Suniel <sunieljolly@googlemail.com>
2022-02-07 08:15:02 +0700

    07-02-2022

--- js/wordledata.js
+++ js/wordledata.js
@@ -13,7 +13,9 @@ const wordledata = [
           {id: 227, lines: 5,  green: 8,  yellow: 7, blank: 10}, 
           {id: 228, lines: 3,  green: 8,  yellow: 3, blank: 4},
           {id: 229, lines: 0,  green: 16,  yellow: 0, blank: 14},   
-          {id: 230, lines: 3,  green: 10,  yellow: 1, blank: 4},       
+          {id: 230, lines: 3,  green: 10,  yellow: 1, blank: 4},  
+          {id: 231, lines: 3,  green: 6,  yellow: 2, blank: 7}, 
+          {id: 232, lines: 3,  green: 9,  yellow: 1, blank: 5},      
       ] 
     },
     { player: "Boysie",
@@ -30,7 +32,9 @@ const wordledata = [
         {id: 227, lines: 3,  green: 5,  yellow: 4, blank: 6},
         {id: 228, lines: 2,  green: 7,  yellow: 2, blank: 1}, 
         {id: 229, lines: 4,  green: 10,  yellow: 3, blank: 7},
-        {id: 230, lines: 4,  green: 11,  yellow: 1, blank: 8},           
+        {id: 230, lines: 4,  green: 11,  yellow: 1, blank: 8},  
+        {id: 231, lines: 4,  green: 10,  yellow: 3, blank: 7},  
+        {id: 232, lines: 4,  green: 15,  yellow: 0, blank: 5},           
     ] 
     },
     { player: "Geoff",
@@ -47,7 +51,9 @@ const wordledata = [
         {id: 227, lines: 3,  green: 7,  yellow: 4, blank: 4},
         {id: 228, lines: 2,  green: 6,  yellow: 2, blank: 2}, 
         {id: 229, lines: 4,  green: 9,  yellow: 2, blank: 9},
-        {id: 229, lines: 2,  green: 7,  yellow: 1, blank: 2},           
+        {id: 230, lines: 2,  green: 7,  yellow: 1, blank: 2},
+        {id: 231, lines: 4,  green: 9,  yellow: 5, blank: 6}, 
+        {id: 232, lines: 4,  green: 7,  yellow: 5, blank: 8},            
     ] 
     },
     { player: "Lee",
@@ -64,7 +70,9 @@ const wordledata = [
         {id: 227, lines: 2,  green: 6,  yellow: 2, blank: 2}, 
         {id: 228, lines: 3,  green: 9,  yellow: 2, blank: 4},
         {id: 229, lines: 4,  green: 14,  yellow: 0, blank: 6},
-        {id: 230, lines: 3,  green: 7,  yellow: 6, blank: 2},           
+        {id: 230, lines: 3,  green: 7,  yellow: 6, blank: 2},  
+        {id: 231, lines: 4,  green: 8,  yellow: 6, blank: 6}, 
+        {id: 232, lines: 3,  green: 8,  yellow: 1, blank: 6},          
     ] 
     },
     { player: "Lloydy",
@@ -81,7 +89,9 @@ const wordledata = [
         {id: 227, lines: 3,  green: 6,  yellow: 4, blank: 5}, 
         {id: 228, lines: 3,  green: 9,  yellow: 2, blank: 4}, 
         {id: 229, lines: 3,  green: 9,  yellow: 1, blank: 5},
-        {id: 230, lines: 4,  green: 7,  yellow: 6, blank: 7},        
+        {id: 230, lines: 4,  green: 7,  yellow: 6, blank: 7}, 
+        {id: 231, lines: 4,  green: 9,  yellow: 4, blank: 7},  
+        {id: 232, lines: 4,  green: 10,  yellow: 1, blank: 9},         
     ]
     },
     { player: "Robbie",
@@ -99,6 +109,8 @@ const wordledata = [
         {id: 228, lines: 3,  green: 8,  yellow: 0, blank: 7},
         {id: 229, lines: 4,  green: 7,  yellow: 7, blank: 6},
         {id: 230, lines: 4,  green: 13,  yellow: 12, blank: 5},
+        {id: 231, lines: 6,  green: 13,  yellow: 3, blank: 14},
+        {id: 232, lines: 6,  green: 15,  yellow: 4, blank: 11},
                   
     ]
     },
@@ -116,7 +128,9 @@ const wordledata = [
         {id: 227, lines: 4,  green: 11,  yellow: 2, blank: 7},
         {id: 228, lines: 3,  green: 8,  yellow: 2, blank: 5}, 
         {id: 229, lines: 4,  green: 9,  yellow: 2, blank: 9},
-        {id: 230, lines: 3,  green: 6,  yellow: 3, blank: 6},          
+        {id: 230, lines: 3,  green: 6,  yellow: 3, blank: 6},
+        {id: 231, lines: 5,  green: 10,  yellow: 5, blank: 10}, 
+        {id: 232, lines: 5,  green: 6,  yellow: 5, blank: 14},           
     ]
     },        
     { player: "Sunny",
@@ -133,7 +147,9 @@ const wordledata = [
         {id: 227, lines: 2,  green: 6,  yellow: 2, blank: 2}, 
         {id: 228, lines: 3,  green: 8,  yellow: 1, blank: 6}, 
         {id: 229, lines: 6,  green: 16,  yellow: 4, blank: 10},
-        {id: 230, lines: 3,  green: 7,  yellow: 6, blank: 2},          
+        {id: 230, lines: 3,  green: 7,  yellow: 6, blank: 2}, 
+        {id: 231, lines: 4,  green: 8,  yellow: 5, blank: 7},
+        {id: 232, lines: 5,  green: 12,  yellow: 3, blank: 10},         
     ] 
     },   
 ]
